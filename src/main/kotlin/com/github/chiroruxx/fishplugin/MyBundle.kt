9efda1eb09fe171b2f400f5ad8f8e18ba961f4c9
GitHub Action <action@github.com>
2022-01-27 06:02:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/chiroruxx/fishplugin/MyBundle.kt
+++ src/main/kotlin/com/github/chiroruxx/fishplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.chiroruxx.fishplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/chiroruxx/fishplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chiroruxx/fishplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chiroruxx.fishplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chiroruxx.fishplugin.MyBundle
 
 class MyApplicationService {
 
